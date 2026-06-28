6186ce1683e3cf00fa8e98c0415570863fd30105
Rakibul Hasan <rakibulhasan4041@gmail.com>
2026-06-29 01:27:58 +0600

    feat: add function to calculate the sum of numbers divisible by 3 from 1 to n

--- main.cpp
+++ main.cpp
@@ -150,3 +150,24 @@
 //     cout << oddSum << endl;
 //     return 0;
 // }
+
+
+
+// //sum of all number from 1 to N, which is divisible by 3
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n = 10;
+
+//     int sum = 0;
+
+//     for(int i=1; i<=n; i++) {
+//         if(i%3 == 0){
+//             sum = sum+i;
+//         }
+//     };
+    
+//     cout << sum << endl;
+//     return 0;
+// }
