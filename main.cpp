9ecc690745e8547639c2cf53d1dd2762389f5f19
Rakibul Hasan <rakibulhasan4041@gmail.com>
2026-06-28 15:45:22 +0600

    feat: add basic variable declarations and additional print statements to main.cpp

--- main.cpp
+++ main.cpp
@@ -4,8 +4,21 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!";
+    cout << "Hello, World!" << endl;
+    cout << "Hello, World!" << "\n";    //new line
+
+    cout << "Rakibul" << "\n" << "Hasan" << endl; 
+
+    int age = 25;               //int 4 byte space taken in memory
+
+    char name = "Hasan";        //int 1 byte space taken in memory
+
+    float PI = 3.1416f;         //int 4 byte space taken in memory
+
+    double price = 100.99;      //int 8 byte space taken in memory
+
+    bool isSafe = true;         //int 1 byte space taken in memory
+
+    cout << age << endl;
     return 0;
 }
-
-
