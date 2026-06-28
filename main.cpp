5d92a8de1b938d6af26ffbb5e22da34afe0dc4c2
Rakibul Hasan <rakibulhasan4041@gmail.com>
2026-06-28 17:51:15 +0600

    feat: update main function to accept user input for age

--- main.cpp
+++ main.cpp
@@ -1,24 +1,39 @@
-//build and run comand: g++ main.cpp && ./a.out
+// //build and run comand: g++ main.cpp && ./a.out
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     cout << "Hello, World!" << endl;
+//     cout << "Hello, World!" << "\n";    //new line
+
+//     cout << "Rakibul" << "\n" << "Hasan" << endl; 
+
+//     int age = 25;               //int 4 byte space taken in memory
+
+//     char name = "Hasan";        //int 1 byte space taken in memory
+
+//     float PI = 3.1416f;         //int 4 byte space taken in memory
+
+//     double price = 100.99;      //int 8 byte space taken in memory
+
+//     bool isSafe = true;         //int 1 byte space taken in memory
+
+//     cout << age << endl;
+//     return 0;
+// }
+
+
 
 #include <iostream>
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
-    cout << "Hello, World!" << "\n";    //new line
+    int age;
 
-    cout << "Rakibul" << "\n" << "Hasan" << endl; 
+    cout << "Enter a number: ";
+    cin >> age;
 
-    int age = 25;               //int 4 byte space taken in memory
-
-    char name = "Hasan";        //int 1 byte space taken in memory
-
-    float PI = 3.1416f;         //int 4 byte space taken in memory
-
-    double price = 100.99;      //int 8 byte space taken in memory
-
-    bool isSafe = true;         //int 1 byte space taken in memory
-
-    cout << age << endl;
+    cout << "Age: " << age << endl;
     return 0;
 }
