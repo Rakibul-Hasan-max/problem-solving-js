682687eed91bd590ba9e3f91530b8759d44b1428
Rakibul Hasan <rakibulhasan4041@gmail.com>
2026-06-29 20:11:01 +0600

    feat: implement various nested loop patterns for character and number printing

--- main.cpp
+++ main.cpp
@@ -217,20 +217,134 @@
 
 
 
-//charecter pattern making using nested loop
+// //charecter pattern making using nested loop
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n = 5;
+
+//     for(int i=1; i<=n; i++) {
+
+//         char ch = 'A';
+
+//         for(int j=1; j<=n; j++) {
+//             cout << ch;
+//             ch = ch +1;
+//         };
+
+//         cout << endl;
+//     };
+    
+//     return 0;
+// }
+
+
+
+
+// //number pattern making using nested loop
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n = 3;
+
+//     int num = 1;
+
+//     for(int i=0; i<n; i++) {
+
+//         for(int j=0; j<n; j++) {
+//             cout << num << " ";
+//             num++;
+//         };
+
+//         cout << endl;
+//     };
+    
+//     return 0;
+// }
+
+
+
+// //charachter pattern making using nested loop
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n = 3;
+
+//     char ch = 'A';
+
+//     for(int i=0; i<n; i++) {
+
+//         for(int j=0; j<n; j++) {
+//             cout << ch << " ";
+//             ch++;
+//         };
+
+//         cout << endl;
+//     };
+    
+//     return 0;
+// }
+
+
+
+// //triangle pattern making using nested loop
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n = 4;
+
+//     for(int i=0; i<n; i++) {        //outer loop
+
+//         for(int j=0; j<i+1; j++) {    //inner loop
+//             cout << "* ";
+//         };
+
+//         cout << endl;
+//     };
+    
+//     return 0;
+// }
+
+
+
+// //triangle pattern making using nested loop
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n = 5;
+
+//     for(int i=0; i<n; i++) {        //outer loop
+
+//         for(int j=0; j<i+1; j++) {    //inner loop
+//             cout << i+1;
+//         };
+
+//         cout << endl;
+//     };
+    
+//     return 0;
+// }
+
+
+
+//triangle pattern making using nested loop
 #include <iostream>
 using namespace std;
 
 int main() {
     int n = 5;
 
-    for(int i=1; i<=n; i++) {
+    char ch = 'A';
 
-        char ch = 'A';
+    for(int i=0; i<n; i++) {        //outer loop
 
-        for(int j=1; j<=n; j++) {
-            cout << ch;
-            ch = ch +1;
+        for(int j=0; j<i+1; j++) {    //inner loop
+            cout << char(ch+i);
         };
 
         cout << endl;
